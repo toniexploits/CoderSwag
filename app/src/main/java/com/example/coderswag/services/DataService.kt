798661cd9d3f0fe9a863de7e492b8658a9485bd0
Toni Olatunji <toniexploits@gmail.com>
2019-09-28 01:53:19 +0100

    create category and product classes, and dataservice singleton

---
+++ app/src/main/java/com/example/coderswag/services/DataService.kt
@@ -0,0 +1,35 @@
+package com.example.coderswag.services
+
+import com.example.coderswag.model.Category
+import com.example.coderswag.model.Product
+
+object DataService {
+    val categories = listOf(
+        Category("SHIRTS", "shirtsimage"),
+        Category("HOODIES", "hoodieimage"),
+        Category("HATS", "hatimage"),
+        Category("DIGITALS", "digitalgoodsimage")
+    )
+
+    val hats = listOf(
+        Product("Devslopes Graphic Beanie", "$18", "hat01"),
+        Product("Devslopes Hat Black", "$25", "hat02"),
+        Product("Devslopes Hat White", "$20", "hat03"),
+        Product("Devslopes Hat Snapback", "$22", "hat04")
+    )
+
+    val hoodies = listOf(
+        Product("Devslopes Hoodie Gray", "$27", "hoodie01"),
+        Product("Devslopes Hoodie Red", "$25", "hoodie02"),
+        Product("Devslopes Hoodie Light Gray", "$24", "hoodie03"),
+        Product("Devslopes Hoodie Black", "$25", "hoodie04")
+    )
+
+    val shirts = listOf(
+        Product("Devslopes Shirt Black", "$20", "shirt01"),
+        Product("Devslopes Badge Light Gray", "$24", "shirt02"),
+        Product("Devslopes Shirt Red", "$18", "shirt03"),
+        Product("Devslopes Hussle", "$22", "shirt04"),
+        Product("Kickflip Studios", "$18", "shirt05")
+    )
+}
